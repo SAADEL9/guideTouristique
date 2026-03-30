5696b36fe8603be1eb391886669a3e104af11ed5
samia elmahi <saadelmahi123@gmail.com>
2026-03-30 14:23:49 +0100

    first commit

---
+++ guideTouristique/src/main/java/com/saad/guideTouristique/GuideTouristiqueApplication.java
@@ -0,0 +1,13 @@
+package com.saad.guideTouristique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideTouristiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideTouristiqueApplication.class, args);
+	}
+
+}
